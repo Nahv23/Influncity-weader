96f52a977d3b3bc1a0ef567ac87171289b81776a
Nerea <nereaparicio@gmail.com>
2019-05-08 21:39:47 +0200

    get the data from the json and print it

--- influencity-weather/src/components/Weather.js
+++ influencity-weather/src/components/Weather.js
@@ -6,40 +6,51 @@ import logo from '../cloudsIcon.png';
 
 class Weather extends Component {
   
+  // constructor(){
+  //   super()
+  //   this.state = {
+  //     data:[]
+  //   }
+  // }
+
+  state={
+    data:[],
+    temp:0
+  }
+  
   componentDidMount (){
     this.getItems();
-  }
-
-  state = {
-    data:''
+    this.getTempInCelsius();
   }
 
   getItems (){
     fetch('https://raw.githubusercontent.com/Influencity/job-offers/master/coding-test/weather-api-2019-04/almeria.json')
       .then( response => response.json())
       .then( response => {
-          this.setState ({data: response})
+          this.setState ({data: response.list})
       })
   }
+
+  getTempInCelsius(kelvin){
+    return Math.round(kelvin-273.15)
+}
   
   render (){
-    const {list}=this.state.data
-    console.log("Respuesta name", list)
     return (
-      <div className="card ml-2 col-2 bg-light border-dark">
+      <div className="card ml-2 col-3 bg-light border-dark">
         <div className="row no-gutters">
           <div className="card-body text-left">
           {this.state.data.map((item, index) => {
             return (
               <div  key={index}>
-                <h5 className="card-title"> {item.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{item.sys.country}</h6>
-                <p className="card-text">{item.main.temp} <span>ºC</span></p>
+                <h4 className="card-title"> {item.name}</h4>
+                <h5 className="card-subtitle mb-2 text-muted">{item.sys.country}</h5>
+                <p className="card-text">{this.getTempInCelsius(item.main.temp)} <span>ºC</span></p>
               </div>
           )} )}
            
           </div>
-          <div className="col-md">
+          <div className="col-md mr-2">
             <img src={logo} className="imageW" alt="weather"></img>
           </div>
         </div>
