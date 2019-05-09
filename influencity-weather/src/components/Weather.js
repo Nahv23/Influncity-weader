29aef7d5cf13a69a7d4060e01fa4e7d8664f2b7d
Nerea <nereaparicio@gmail.com>
2019-05-09 12:04:26 +0200

    get data

--- influencity-weather/src/components/Weather.js
+++ influencity-weather/src/components/Weather.js
@@ -5,33 +5,48 @@ import logo from '../cloudsIcon.png';
 class Weather extends Component {
 
   state={
-    data:[],
-    temp:0
-  }
-  
-  componentDidMount (){
-    this.getItems();
-    this.getTempInCelsius();
+    data:{
+      city:[]
+    },
+    temp:0,
+    cities: ["almeria", "granada", "malaga", "jaen", "cordoba", "sevilla", "huelva", "cadiz"]
   }
 
-  getItems (){
-    fetch('https://raw.githubusercontent.com/Influencity/job-offers/master/coding-test/weather-api-2019-04/almeria.json')
+  
+  componentDidMount (){
+    this.state.cities.map(city => {
+      this.getItems(city);
+      this.getTempInCelsius();
+    })
+
+  }
+
+  getItems (city){
+    let url = "https://raw.githubusercontent.com/Influencity/job-offers/master/coding-test/weather-api-2019-04/"+city+".json";
+    fetch(url)
       .then( response => response.json())
       .then( response => {
-          this.setState ({data: response.list})
+        this.setState({
+          data: {
+            ...this.state.data,
+            [city]: response.list
+          }
+        })
       })
-  }
+}
 
   getTempInCelsius(kelvin){
     return Math.round(kelvin-273.15)
 }
   
   render (){
+    console.log("QUE ES- >", this.state.data, "TIPO-> ", typeof(this.state.data))
+    const {data} =this.state
     return (
       <div className="card ml-2 col-3 bg-light border-dark">
         <div className="row no-gutters d-flex">
           <div className="card-body text-left">
-          {this.state.data.map((item, index) => {
+          {data.city.map((item, index) => {
             return (
               <div  key={index}>
                 <h4 className="card-title"> {item.name}</h4>
